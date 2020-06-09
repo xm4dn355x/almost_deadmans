bfffdb09b57dd25628fdebbb88d02a18072ec387
Michael Nikitenko <xm4dn355x@gmail.com>
2020-06-09 17:53:54 +0500

    made func for crutch)))

--- sandbox.py
+++ sandbox.py
@@ -11,7 +11,27 @@
 
 import xlrd
 import xlwt
+from pprint import pprint
+
+
+def get_xls_data():
+    res = []
+    sheet = xlrd.open_workbook('input.xlsx').sheet_by_index(0)
+    for rownum in range(sheet.nrows):
+        rw = sheet.row_values(rownum)
+        try:
+            n, nm, tl, tl_d, vs, vm, vb, mob, ct, etc, idiots = int(rw[0]), rw[1].split(', род. ')[0], rw[2], rw[3], \
+                                                                rw[4], rw[5], rw[6], rw[7], rw[8], rw[9], rw[10]
+            try:
+                date = rw[1].split(', род. ')[1]
+            except IndexError:
+                date = ''
+            res.append([n, nm, date, tl, tl_d, vs, vm, vb, mob, ct, etc, idiots])
+        except ValueError:
+            res.append(rw)
+    return res
 
 
 if __name__ == '__main__':
-    pass
+    xls_data = get_xls_data()
+    pprint(xls_data)
